1760d9e60ef2d12666b116c2b77563ba3b8b5033
sanei1509 <santiagoneira52@gmail.com>
2022-03-12 04:02:19 +0000

    fix conflictsvi

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
-
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	return (0);
